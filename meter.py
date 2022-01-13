56c3e04e9c6629d47772dd2c24864a40b0d988f2
David Plaskett <dcplaskett@gmail.com>
2022-01-13 14:38:06 -0600

    Update to censored

--- meter.py
+++ meter.py
@@ -4,8 +4,8 @@ import pyinputplus as pyip
 
 #url = 'https://einfo.gflesch.com/einfo//aem.aspx?ac=2976643-9556'
 url = pyip.inputStr(prompt='Enter URL: ')
-jonesurl = 'http://10.7.40.106/machine_status.html'
-busurl = 'http://10.8.40.3/machine_status.html'
+jonesurl = 'http://censored/machine_status.html'
+busurl = 'http://censored/machine_status.html'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
@@ -39,4 +39,4 @@ busInput.send_keys(Keys.SPACE)
 browser.find_element_by_id('newReading').send_keys(busCount)
 browser.find_element_by_xpath('//button[text()="OK"]').click()
 
-#browser.find_element_by_id('btnSubmit2').click()
+browser.find_element_by_id('btnSubmit2').click()
